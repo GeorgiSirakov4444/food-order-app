6752e4933e98a0ff809d4c953a23dbb5d44006d8
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-11-11 08:43:24 +0200

    Creating the basic components, and connections.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
-import healthyMeal from '../../assets/food.jpeg';
+import healthyMeal from '../../assets/buffet.jpeg';
+import HeaderCartButton from "./HeaderCartButton";
 import classes from './Header.module.css';
 
 const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>Food App</h1>
-            <button>Cart</button>
+            <HeaderCartButton/>
         </header>
         <div className={classes['main-image']}>
             <img src={healthyMeal} alt='Table with healthy foods!'/>
