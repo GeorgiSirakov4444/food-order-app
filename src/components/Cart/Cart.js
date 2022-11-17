56c5592c9aeca46e632afceabd72f86cb9f1f1a8
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-11-17 12:51:03 +0200

    adding reducer logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,28 +1,45 @@
-import React, {} from 'react';
-import classes from './Cart.module.css';
+import React, {useContext} from 'react';
 import Modal from "../UI/Modal";
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = props => {
+    const cartCtx = useContext(CartContext);
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartAddHandler = item => {
+
+    };
+
+    const cartRemoveHandler = id => {
+
+    };
 
     const cartItems = <ul className={classes['cart-items']}>
-        {[{
-            id: 'c1',
-            name: 'pushi',
-            amount: 4,
-            price: 3.99
-        }].map((item) => <li>{item.name}</li>)}
+        {cartCtx.items.map((item) => (
+            <CartItem 
+                key={item.id}
+                name={item.name}
+                amount={item.amount}
+                price={item.price}
+                onRemove={cartRemoveHandler.bind(null, item.id)}
+                onAdd={cartAddHandler.bind(null, item)}
+                />
+        ))}
     </ul>
 
     return (
         <Modal onHideCart7={props.onHideCart7}>
             {cartItems}
             <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>35.64</span>
+                <span>Total Amount:</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onHideCart7}>Close</button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
